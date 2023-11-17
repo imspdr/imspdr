03d9cd609caf964cfeb3612feecc5c5602cf2b37
Seungho Lee <seungho_lee2@tmax.co.kr>
2023-11-17 17:42:18 +0900

    minor

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# imspdr develop practice
+# imspdr React, javascript 연습용 토이 프로젝트 모음
--- front/webpack.config.js
+++ front/webpack.config.js
@@ -25,7 +25,6 @@ module.exports = {
     new CleanWebpackPlugin(),
     new ReactRefreshWebpackPlugin(),
   ],
-  mode: "development",
   devtool: "eval-source-map",
   module: {
     rules: [
