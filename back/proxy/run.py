7435dc560b783d220395596ee8478238ab87501a
imspdr <konglsh@naver.com>
2023-12-02 22:14:13 +0900

    minor

--- back/proxy/run.py
+++ back/proxy/run.py
@@ -4,7 +4,9 @@ from apis.p3_riot import p3_riot
 app = Flask(__name__)
 app.register_blueprint(p3_riot)
 
-##############################################################################
+@app.route("/")
+def index():
+    return "hello imspdr"
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=5000, debug=True)
+    app.run(port=5000, debug=True)
